c37a0f138b1f80098e769cf8d97432a5a02733ca
Khang <khangkhoailang20@gmail.com>
2023-04-06 17:56:23 +0700

    test push

---
+++ Web_dienthoai/Web-dienthoai/Models/ProductView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_dienthoai.Models
+{
+    public class ProductView
+    {
+        public SanPham SanPham { get; set; }
+
+        public HinhSP HinhSP { get; set; } 
+
+
+    }
+}
